c417a2074026954fe515c7794d4b2e6dbbcfd73e
Puneet1 <sahota.puneet@gmail.com>
2020-05-09 01:34:34 -0400

    refactoring of the code

--- Hello.py
+++ Hello.py
@@ -1,8 +1,7 @@
 # This program prints Hello, world!
+# In Version two of the code let's accept input from the user
 
-print('Hello, world!')
-print ('Python is analytical in nature')
-x=2
-y=5
-z=x+y
-print ("The sum of numbers is ",z)
+print ("Hello and welcome to Python")
+var_x=raw_input("Please enter the value of x:")
+var_y=raw_input("Please enter the value of y:")
+print("The sum of x and y is :",var_x+var_y)
