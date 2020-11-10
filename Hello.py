f6367c576ed813cc5c256cdd0594acffaad9dad2
Puneet1 <sahota.puneet@gmail.com>
2020-11-10 11:01:42 -0500

    added another print statement to the file

--- Hello.py
+++ Hello.py
@@ -6,3 +6,4 @@ var_x=input("Please enter the value of x:") #returns a string
 var_y=input("Please enter the value of y:") #returns a string
 var_z=float(var_x)+float(var_y)
 print("The sum of {0} and {1} is : {2}".format(var_x, var_y, var_z))
+print("Hello Python World")
